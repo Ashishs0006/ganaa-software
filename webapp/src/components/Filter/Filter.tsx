ef4106848ad454dec5d1eda5fbd6d8e4f4b93ba1
Harshit sharma <programming966051@gmail.com>
2025-11-17 15:26:54 +0530

    working on followup and other pages

--- webapp/src/components/Filter/Filter.tsx
+++ webapp/src/components/Filter/Filter.tsx
@@ -1,64 +1,15 @@
-import { useMemo } from "react";
-import { useAuth } from "@/providers/AuthProvider";
-import SingleSelect from "../MultiSelect/MultiSelect";
-import { useSearchParams } from "react-router-dom";
-
-const Filter = () => {
-  const { auth } = useAuth();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const selected = searchParams.get("filter") || "All";
-
-  const centerDropdown = useMemo(() => {
-    const centerList = auth.user.centerId ?? [];
-
-    const allOption = centerList.length > 1 ? [{ value: "All", label: "All" }] : [];
-
-    const mappedCenters = centerList.map(
-      ({ centerName, _id }: { centerName: string; _id: string }) => ({
-        label: centerName,
-        value: _id
-      })
-    );
-
-    return [...allOption, ...mappedCenters];
-  }, [auth.user.centerId]);
-
-  const handleselected = (values: string) => {
-    searchParams.set("filter", values);
-    setSearchParams(searchParams);
-  };
-
-  return (
-    <>
-      <SingleSelect
-        options={centerDropdown}
-        selectedValue={selected}
-        onChange={handleselected}
-        placeholder={centerDropdown?.length > 1 ? "All" : centerDropdown[0]?.label}
-      />
-    </>
-  );
-};
-
-export default Filter;
-
-
 // import { useMemo } from "react";
 // import { useAuth } from "@/providers/AuthProvider";
 // import SingleSelect from "../MultiSelect/MultiSelect";
+// import { useSearchParams } from "react-router-dom";
 
-// const Filter = ({
-//   selected = "All",
-//   setSelected
-// }: {
-//   selected?: string;
-//   setSelected?: (_values: string) => void;
-// }) => {
+// const Filter = () => {
 //   const { auth } = useAuth();
+//   const [searchParams, setSearchParams] = useSearchParams();
+//   const selected = searchParams.get("filter") || "All";
 
 //   const centerDropdown = useMemo(() => {
 //     const centerList = auth.user.centerId ?? [];
-//     console.log('centerList: ', centerList);
 
 //     const allOption = centerList.length > 1 ? [{ value: "All", label: "All" }] : [];
 
@@ -72,12 +23,17 @@ export default Filter;
 //     return [...allOption, ...mappedCenters];
 //   }, [auth.user.centerId]);
 
+//   const handleselected = (values: string) => {
+//     searchParams.set("filter", values);
+//     setSearchParams(searchParams);
+//   };
+
 //   return (
 //     <>
 //       <SingleSelect
 //         options={centerDropdown}
 //         selectedValue={selected}
-//         onChange={setSelected ?? (() => {})}
+//         onChange={handleselected}
 //         placeholder={centerDropdown?.length > 1 ? "All" : centerDropdown[0]?.label}
 //       />
 //     </>
@@ -85,3 +41,47 @@ export default Filter;
 // };
 
 // export default Filter;
+
+
+import { useMemo } from "react";
+import { useAuth } from "@/providers/AuthProvider";
+import SingleSelect from "../MultiSelect/MultiSelect";
+
+const Filter = ({
+  selected = "All",
+  setSelected
+}: {
+  selected?: string;
+  setSelected?: (_values: string) => void;
+}) => {
+  const { auth } = useAuth();
+
+  const centerDropdown = useMemo(() => {
+    const centerList = auth.user.centerId ?? [];
+    console.log('centerList: ', centerList);
+
+    const allOption = centerList.length > 1 ? [{ value: "All", label: "All" }] : [];
+
+    const mappedCenters = centerList.map(
+      ({ centerName, _id }: { centerName: string; _id: string }) => ({
+        label: centerName,
+        value: _id
+      })
+    );
+
+    return [...allOption, ...mappedCenters];
+  }, [auth.user.centerId]);
+
+  return (
+    <>
+      <SingleSelect
+        options={centerDropdown}
+        selectedValue={selected}
+        onChange={setSelected ?? (() => {})}
+        placeholder={centerDropdown?.length > 1 ? "All" : centerDropdown[0]?.label}
+      />
+    </>
+  );
+};
+
+export default Filter;
